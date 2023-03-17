06f2f66bbb7c0007d1b962c3013dc7d9329eaf98
chen <37500268+chenxygh@users.noreply.github.com>
2023-03-17 21:08:55 +0800

    修改 clash.zsh 加载文件的相对路径为绝对路径

--- clash/clash.zsh
+++ clash/clash.zsh
@@ -1,6 +1,7 @@
 #/bin/zsh
 
-. ./proxy.zsh
-. ./clash_func.zsh
+DOTFILES=$HOME/.local/.Cdotfiles
+. $DOTFILES/proxy.zsh
+. $DOTFILES/clash_func.zsh
 export PATH=$HOME/.local/bin:$PATH
 alias clash="nohup clash > /dev/null 2>&1 &"
