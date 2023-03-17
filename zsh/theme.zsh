eb0bae23f13e3bde0ab446d133cd02fd07e24b1e
chen <37500268+chenxygh@users.noreply.github.com>
2023-03-17 21:12:30 +0800

    修改 theme.zsh, 复制改为加载 .p10k.zsh

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,8 +3,9 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
+DOTFILES=$HOME/.local/.Cdotfiles
 zinit ice depth=1; zinit light romkatv/powerlevel10k
-cp ./.p10k.zsh $HOME
+. $DOTFILES/.p10k.zsh
 
 zinit light zsh-users/zsh-autosuggestions
 zinit load zdharma-continuum/history-search-multi-word
